df2e2092957178cd056e3f23e85736619d7c453f
Documenter.jl <documenter@juliadocs.github.io>
2022-09-16 03:23:25 +0000

    build based on 5cf848b

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = JlSensemakr","category":"page"},{"location":"#JlSensemakr","page":"Home","title":"JlSensemakr","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for JlSensemakr.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [JlSensemakr]","category":"page"}]
+}
