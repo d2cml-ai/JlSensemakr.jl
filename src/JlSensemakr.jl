d4410ca956f6079969afc21a110efb4c14d68b0e
RodrigoGrijalba <ragrijalba119@gmail.com>
2022-09-20 21:02:58 -0500

    module build

--- src/JlSensemakr.jl
+++ src/JlSensemakr.jl
@@ -1,5 +1,9 @@
 module JlSensemakr
 
-# Write your package code here.
+export robustness_value, partial_r2, partial_f2, partial_f, group_partial_r2, sensitivity_stats
+
+using Distributions, DataFrames, GLM
+
+include("sensitivity_statistics.jl")
 
 end
