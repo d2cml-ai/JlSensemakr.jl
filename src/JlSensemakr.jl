f84dac65ddcaa7ab02c8c23f645422c969762fe9
RodrigoGrijalba <ragrijalba119@gmail.com>
2022-10-06 20:39:55 -0500

    plot fix

--- src/JlSensemakr.jl
+++ src/JlSensemakr.jl
@@ -4,10 +4,10 @@ export robustness_value, partial_r2, partial_f2, group_partial_r2, sensitivity_s
 
 using Distributions, DataFrames, GLM, StatsModels, PyPlot
 
+include("Sensemakr.jl")
 include("sensitivity_statistics.jl")
 include("bias_functions.jl")
 include("sensitivity_bounds.jl")
 include("sensitivity_plots.jl")
-include("Sensemakr.jl")
 
 end
